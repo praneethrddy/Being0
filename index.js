1fdaf309a5ae02a573e02d07cabd006eab3b3e5b
praneethrddy <praneethreddy0601@gmail.com>
2019-12-19 12:58:28 +0530

    (NEW PROJECT)

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.send('Pani Chusko ra BULLE😎😎'))
+app.use('/static', express.static('public'))
+app.get('/', (req, res) => res.sendfile(__dirname + '/public/htmls/index.html'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
