71a73f7f409f5c5b3ec057b7c04eb2cd251a4922
praneethrddy <praneethreddy0601@gmail.com>
2019-12-19 14:14:18 +0530

    fixed static file path

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use('/static', express.static('public'))
-app.get('/', (req, res) => res.sendfile(__dirname + '/public/htmls/index.html'))
+app.use(express.static('public'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/', (req, res) => res.sendFile(__dirname + '/public/htmls/index.html'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
