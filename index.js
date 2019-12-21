85777e5b739cffe9f2ec8594aa3f3b0141e9ca1b
praneethrddy <praneethreddy0601@gmail.com>
2019-12-21 10:56:20 +0530

    DOne changes

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.static('public'))
-app.get('/', (req, res) => res.sendfile(__dirname +'/public/htmls/index.html'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/', (req, res) => res.sendFile(__dirname + '/public/htmls/index.html'))
+
+var obj={}
+obj.name = "Praneeth Reddy"
+obj.collage=" CMRCET"
+obj.regno= "ABD34s"
+app.get('/data', (req, res) => res.json(obj))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
