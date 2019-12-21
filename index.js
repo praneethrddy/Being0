588085dd96fa4ec2bed00e03e29c26b4c629d9d9
praneethrddy <praneethreddy0601@gmail.com>
2019-12-21 10:46:05 +0530

    At HOME

--- index.js
+++ index.js
@@ -2,14 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(express.static('public'))
+app.use('/static', express.static('public'))
+app.get('/', (req, res) => res.sendfile(__dirname +/public/htmls/index.html))
 
-app.get('/', (req, res) => res.sendFile(__dirname + '/public/htmls/index.html'))
-
-var obj={}
-obj.name = "Praneeth Reddy"
-obj.collage=" CMRCET"
-obj.regno= "ABD34s"
-app.get('/data', (req, res) => res.json(obj))
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
